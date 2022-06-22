3856cdcc00a786f4a4488fa47e25f7a569106aea
Sayantan Mandal <83362170+SayantanMandal2000@users.noreply.github.com>
2022-06-22 18:46:52 +0530

    Create testbench.sv

---
+++ Count99/testbench.sv
@@ -0,0 +1,25 @@
+`timescale 1ns/1ps
+
+module testbench;
+  
+  reg clk,rst;
+  wire [3:0]q;
+  
+  counter UUT(clk,rst,q);
+  
+  initial begin
+    clk=0;
+    rst=0;
+    #10 rst=1;
+    #10 rst=0;
+    
+    #1 $monitor(" q = %d ",q);
+    #500 $stop;
+  end
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1,testbench);
+  end
+  
+endmodule
